85c8164584c31e8a8071e873918f3b68c9bc0a0f
canberkyasa <canberkyasa0@gmail.com>
2023-01-04 14:28:23 +0300

    Ödev 1

---
+++ Ödev 1/odev_1.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+var sehir = "Ankara"
+var ulke = "Hollanda"
+var telefon = "+90 544 544 54 44"
+var email = "google@gmail.com"
+var meslek = "Mühendis"
+var stokMiktari = 398
+var bakiye = 88.50
+var dogumGunu = "13.03.2005"
+var maas = 18500
+var medeniDurum = "B"
+var urunYorum = "Çok beğendim, yine alırım"
+var odemeTarihi = "10.10.2022"
+var odeme = 33.99
+var siparisAdeti = 2
+var arabaModeli = "Mercedes"
+var kitapAdi = "Dorian Gray'in Portresi"
+var yayinTarihi = "12.02.2023"
+var indirimMiktari = 0.50
+var enlem = 33
+var boylam = 59
+var urunAdi = "Herby Uyku Çayı"
+var yemekFiyati = 54
+var marka = "Adidas"
+var muzikAdi = "Those Eyes by New West"
+var videoSuresi = 33.42
+var urunPuani = 4.7
+var resimAdi = "Mona Lisa"
+var dosyaFormati = "jpeg"
+var renk = "mavi"
+var renkKodu = "#98233"
+var telefonModeli = "Iphone 13"
+var ekranBoyutu = "1350/1080"
+var agirlik = 78
+var ulusalGun = 8
+var tatilGunu = 21
+var rezervasyanTarihi = "11.01.2023"
+var sokakAdi = "Kızılcık"
+var otobusHatti = 504
+var kalanDakika = 33
+var takipKodu = 239832
+var kuponSuresi = 21
+var kuponKodu = "BIBER10"
+var faturaAdresi = "Maltepe mah. Gülseren Sokak 33/02"
+
+print(agirlik)

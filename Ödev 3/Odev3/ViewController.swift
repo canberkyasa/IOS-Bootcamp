b21268370951400cd936d0e7912450f7bc749bd8
canberkyasa <canberkyasa0@gmail.com>
2023-01-15 03:29:24 +0300

    Ödev 3

---
+++ Ödev 3/Odev3/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Odev3
+//
+//  Created by Canberk Yaşa on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
